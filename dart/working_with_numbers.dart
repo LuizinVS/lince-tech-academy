b4ed0b78d78628426fd8e8ea7b19d51c03ca8a2b
LuizinVS <luiz.mariath3@gmail.com>
2026-07-01 17:53:27 -0300

    new classes files and notations

---
+++ dart/working_with_numbers.dart
@@ -0,0 +1,5 @@
+void main() {
+  double salario = 1200.90;
+  salario.round();
+  print(salario);
+}
---
+++ dart/working_with_text.dart
@@ -0,0 +1,6 @@
+void main(){
+  String string1 = 'its text';
+  String string2 = "Estou estudando" " Dart";
+  String linguagem = "Dart";
+  String string3 = "Estou estudando $linguagem";
+}
