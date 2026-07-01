397967dcb83de8fde5a0fd2f457b080eca6293b7
LuizinVS <luiz.mariath3@gmail.com>
2026-06-30 22:16:19 -0300

    testando diferentes variaveis

--- dart/working_with_variables.dart
+++ dart/working_with_variables.dart
@@ -1,4 +1,10 @@
 void main(){
     var nameCompleto = 'Jose da Silva';
+    int idade = 10;
+    double salario = 1616.10;
+    bool isAlive = true;
     print (nameCompleto);
+    print (idade);
+    print (salario);
+    print (isAlive);
 }
